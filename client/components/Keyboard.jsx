8d0a98691796afd1ac18d37842456be4bce7799f
Veeta Orinko <107618223+veeta-orinko@users.noreply.github.com>
2022-09-11 14:26:38 +1200

    useState and hooks for one test key event

--- client/components/Keyboard.jsx
+++ client/components/Keyboard.jsx
@@ -1,31 +1,14 @@
-import React from 'react'
-
-window.addEventListener('keydown', keypress, false)
-
-function keypress(e) {
-  if (e.keyCode == '27') {
-    alert('esc has been pressed')
-  }
-}
-/// making the keyboard images responsive to key press
+import React, { useState } from 'react'
 
 const Keyboard = () => {
-  return keypress
+  window.addEventListener('keydown', keypress, false)
+  const [img, setImg] = useState('')
+  function keypress(e) {
+    if (e.keyCode == '27') {
+      setImg(<img src="../images/27keyDown.png" alt="escape key pressed" />)
+    }
+  }
+  return <div>{img}</div>
 }
 
 export default Keyboard
-
-// const escKey = <img src="../images/esc.png" alt="escape key no event"></img>
-
-// const Keyboard = () => {
-//   function keypress(e) {
-//     if (e.keyCode == '27') {
-//       return <img src="../images/esckeyup.png" alt="escape key pressed"></img>
-//     } else {
-//       return { escKey }
-//     }
-//   }
-//   return console.log(keypress)
-// }
-
-// export default Keyboard
