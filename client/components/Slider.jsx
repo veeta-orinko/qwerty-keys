247ef95504a21aa04d26ac7769c5470015d9d63e
Veeta Orinko <107618223+veeta-orinko@users.noreply.github.com>
2023-05-31 15:54:05 +1200

    test

--- client/components/Slider.jsx
+++ client/components/Slider.jsx
@@ -1,54 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { Slider, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import Slider from '@mui/material/Slider';
 import * as Tone from 'tone';
 
-let synth = new Tone.Synth().toDestination();
-let volume = new Tone.Volume(0);
-let distortion = new Tone.Distortion(0);
-synth.chain(volume, distortion, Tone.Destination);
-
 function DistortionSlider() {
-  const [distValue, setDistValue] = useState(0);
-  const [volValue, setVolValue] = useState(0);
+  const [volume, setVolume] = useState(0);
+  const [distortion, setDistortion] = useState(0);
 
-  useEffect(() => {
-    distortion.distortion = distValue;
-    volume.volume.value = volValue;
-  }, [distValue, volValue]);
+  const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+  const volumeControl = new Tone.Volume(volume);
+  const distortionEffect = new Tone.Distortion(distortion);
 
-  const handleDistChange = (event, newValue) => {
-    setDistValue(newValue);
+  synth.chain(distortionEffect, volumeControl);
+
+  const handleVolumeChange = (event, newValue) => {
+    setVolume(newValue);
+    volumeControl.volume.value = newValue;
   };
 
-  const handleVolChange = (event, newValue) => {
-    setVolValue(newValue);
+  const handleDistortionChange = (event, newValue) => {
+    setDistortion(newValue);
+    distortionEffect.distortion = newValue;
   };
 
   return (
-    <>
-      <Typography id="dist-slider" gutterBottom>
-        Distortion
-      </Typography>
-      <Slider
-        value={distValue}
-        min={0}
-        max={1}
-        step={0.01}
-        onChange={handleDistChange}
-        aria-labelledby="dist-slider"
-      />
-      <Typography id="vol-slider" gutterBottom>
-        Volume
-      </Typography>
-      <Slider
-        value={volValue}
-        min={-60}
-        max={0}
-        step={1}
-        onChange={handleVolChange}
-        aria-labelledby="vol-slider"
-      />
-    </>
+    <div>
+      <div>
+        <h2>Volume</h2>
+        <Slider value={volume} onChange={handleVolumeChange} />
+      </div>
+      <div>
+        <h2>Distortion</h2>
+        <Slider value={distortion} onChange={handleDistortionChange} />
+      </div>
+    </div>
   );
 }
 
