ed34ee286dec99303f119900aac8eee823175b97
Veeta Orinko <107618223+veeta-orinko@users.noreply.github.com>
2023-05-31 16:53:24 +1200

    test

--- client/components/Slider.jsx
+++ client/components/Slider.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as Tone from 'tone';
-import Slider from '@mui/material/Slider';
 
-// Create and export the synth and distortion effect in a separate module
-// This allows the same synth and distortion effect to be used in multiple components
-export const synth = new Tone.PolySynth(Tone.Synth).toDestination();
-export const distortionEffect = new Tone.Distortion(0);
-
-// The Slider component controls the amount of distortion
 function DistortionSlider() {
-  const [distortion, setDistortion] = React.useState(0);
+    // Create a Tone.js distortion effect with an initial value
+    const distortion = useRef(new Tone.Distortion(0.4));
 
-  const handleDistortionChange = (event, newValue) => {
-    setDistortion(newValue);
-    distortionEffect.distortion = newValue;
-  };
+    // This function is called when the slider's value changes
+    const handleSliderChange = (event) => {
+        // Convert the slider value from string to float
+        const value = parseFloat(event.target.value);
 
-  return (
-    <Slider value={distortion} onChange={handleDistortionChange} min={0} max={1} step={0.01} />
-  );
+        // Set the distortion amount. You may need to scale or adjust this depending
+        // on the range and scale of your slider.
+        distortion.current.distortion = value;
+    }
+
+    // When the component is mounted, connect the distortion effect to the master output
+    useEffect(() => {
+        distortion.current.toDestination();
+        return () => {
+            // Disconnect and dispose of the distortion effect when the component is unmounted
+            distortion.current.disconnect();
+            distortion.current.dispose();
+        }
+    }, []);
+
+    return (
+        <div>
+            <input type="range" min="0" max="1" step="0.01" onChange={handleSliderChange} />
+        </div>
+    )
 }
-
 
 export default DistortionSlider;
