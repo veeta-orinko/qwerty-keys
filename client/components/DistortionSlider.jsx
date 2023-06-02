7b8d045b50c679977458ea436f859d8bae85bb0b
Veeta Orinko <107618223+veeta-orinko@users.noreply.github.com>
2023-06-02 12:19:25 +1200

    working, dials configured except tremolo

--- client/components/DistortionSlider.jsx
+++ client/components/DistortionSlider.jsx
@@ -1,20 +1,21 @@
 import React from 'react';  
-import { distortion } from './Synth.jsx'; // Import the distortion from Synth.jsx
+import { distortion } from './Synth.jsx'; // Import the distortion from Synth.jsx  
 
 function DistortionSlider() {  
-  // This function is called when the slider's value changes  
+  // This function is called when the slider's value changes   
   const handleSliderChange = (event) => {  
-    // Convert the slider value from string to float  
+    // Convert the slider value from string to float   
     const value = parseFloat(event.target.value);  
 
-    // Set the distortion amount. You may need to scale or adjust this depending  
-    // on the range and scale of your slider.  
-    distortion.wet.value = value;
-}  
+    // Set the distortion amount. You may need to scale or adjust this depending   
+    // on the range and scale of your slider.   
+    distortion.distortion = value;  
+  }  
 
   return (  
     <div>  
       <input type="range" min="0" max="1" step="0.01" onChange={handleSliderChange} />  
+      <label> Distortion </label>
     </div>  
   )  
 }  
--- client/components/TremoloSlider.jsx
+++ client/components/TremoloSlider.jsx
@@ -1,16 +1,16 @@
 import React from 'react';  
-import { tremolo } from './Synth.jsx';
+import { tremolo } from './Synth.jsx';  
 
 function TremoloSlider() {  
   const handleSliderChange = (event) => {  
-    const value = parseFloat(event.target.value);
-    tremolo.wet.value = value;
-  }
+    const value = parseFloat(event.target.value);  
+    tremolo.frequency.value = value;  
+  }  
 
   return (  
     <div>  
-      <input type="range" min="0" max="1" step="0.01" onChange={handleSliderChange} />
-      <label>Tremolo</label>
+      <input type="range" min="0" max="10" step="0.01" onChange={handleSliderChange} />  
+      <label>Tremolo Frequency</label>  
     </div>  
   )  
 }  
